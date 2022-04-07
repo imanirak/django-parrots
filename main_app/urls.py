59e65f92de1744a672fcf618c739edc72451f257
imanirak <karinadgarces@gmail.com>
2022-04-06 23:34:03 -0400

    imported parrot data

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
     path('', views.Home.as_view(), name="home"), # <- here we have added the new path
-    path('about/', views.About.as_view(), name="about")
+    path('about/', views.About.as_view(), name="about"),
+    path('parrots/', views.Parrot_List.as_view(), name='parrot_list')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from pipes import Template
 from django.shortcuts import render
 from django.urls import path
 from . import views
@@ -8,6 +9,7 @@ from django.http import HttpResponse # <- a class to handle sending a type of re
 from django.views.generic.base import TemplateView
 
 # Create your views here.
+
 
 # Here we will be creating a class called Home and extending it from the View class
 class Home(TemplateView):
@@ -20,3 +22,41 @@ class Home(TemplateView):
     
 class About(TemplateView):
     template_name='about.html'
+    
+class Parrot:
+    def __init__(self, name):
+        self.name = name
+    
+
+parrots = [
+    Parrot("African Grey"),
+    Parrot("Cockatoos"),
+    Parrot("Macaws"),
+    Parrot("Parrotlet"),
+    Parrot("Cockatiel"),
+    Parrot("Senegal"),
+    Parrot("Parakeets"),
+    Parrot("Eclectus"),
+    Parrot("Amazon"),
+    Parrot("Pionus"),
+    Parrot("Conure"),
+    Parrot("Burrowing"),
+    Parrot("Caique"),
+    Parrot("Yellow-Naped Amazon"),
+    Parrot("Meyer's"),
+    Parrot("Ring-Necked Parakeet"),
+    Parrot("Kakariki"),
+    Parrot("Timneh Greys"),
+    Parrot("Quaker"),
+    Parrot("Lovebirds")
+           
+           ]
+
+class Parrot_List(TemplateView):
+    template_name= 'parrot_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['parrots'] = parrots
+        
+        return context
