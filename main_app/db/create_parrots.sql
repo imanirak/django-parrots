fe567ff4407837d10305046509b716dc8752dd57
imanirak <karinadgarces@gmail.com>
2022-04-12 16:22:54 -0400

    full crud

--- main_app/db/create_parrots.sql
+++ main_app/db/create_parrots.sql
@@ -1,8 +1,8 @@
 INSERT INTO main_app_parrot(name, img, created_at)
-VALUES ('Senegal', 'https://www.thesprucepets.com/thmb/gKjqRGgRcs8a2o-_r8-HmTA9V5A=/3456x3456/smart/filters:no_upscale()/GettyImages-539644413-58a6f2263df78c345b631198.jpg','03-19-2022');
+VALUES ('Senegal', '','03-19-2022');
 INSERT INTO main_app_parrot(name, img, created_at)
-VALUES ('African Gray', 'https://cff2.earth.com/uploads/2021/10/20180948/African-grey-parrot-scaled.jpg','03-19-2022');
+VALUES ('African Gray', '','03-19-2022');
 INSERT INTO main_app_parrot(name, img,  created_at)
-VALUES ('Sun Conure', 'https://media-be.chewy.com/wp-content/uploads/conure_thawats.jpg','03-19-2022');
-INSERT INTO main_app_parrot(name, img, created_at)
-VALUES ('Green Wing Macaw', 'https://www.thesprucepets.com/thmb/HUGsHaiDVgEKKGwb1Lo5tfPrWy0=/3456x3456/smart/filters:no_upscale()/pair-of-red-and-green-macaws--ara-chloropterus--interacting-together--jardim--mato-grosso-do-sul--brazil-740534161-5b5b7ddd46e0fb005028d9f5.jpg', '03-19-2022');
+VALUES ('Sun Conure', '','03-19-2022');
+INSERT INTO main_app_parrot(name, img, created_at) 
+VALUES ('Green Wing Macaw', '' ,'03-19-2022');
--- main_app/urls.py
+++ main_app/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('parrots/', views.Parrot_List.as_view(), name='parrot_list'),
     path('parrots/new/', views.ParrotCreate.as_view(), name="parrot_create"),
     path('parrots/<int:pk>/', views.Parrot_Detail.as_view(), name="parrot_detail"),
+    path('cats/<int:pk>/update', views.Parrot_Update.as_view(), name="parrot_update"),
+    path('cats/<int:pk>/delete', views.Parrot_Delete.as_view(), name="parrot_delete"),
     path('user/<username>', views.profile , name="profile")
 ]
