2d157c762953750d63eb1ba129ea024f12a217d8
imanirak <karinadgarces@gmail.com>
2022-04-12 15:33:26 -0400

    everything functional

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"), # <- here we have added the new path
     path('about/', views.About.as_view(), name="about"),
     path('parrots/', views.Parrot_List.as_view(), name='parrot_list'),
-    path('user/<username>', views.profile, name="profile")
+    path('parrots/new/', views.ParrotCreate.as_view(), name="parrot_create"),
+    path('parrots/<int:pk>/', views.Parrot_Detail.as_view(), name="parrot_detail"),
+    path('user/<username>', views.profile , name="profile")
 ]
